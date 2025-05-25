e985af120c0f8d95f27a2b10d877faa94ca1096a
evanderlucherris <evanderlucherris@gmail.com>
2025-05-25 16:22:45 -0300

    update

--- Mundo_3/ex082_Dividing_values_into_multiple_lists.py
+++ Mundo_3/ex082_Dividing_values_into_multiple_lists.py
@@ -5,3 +5,20 @@ After that, create two additional lists that will contain only the even and the 
 At the end, display the contents of all three generated lists.
 '''
 
+num = []
+pares = []
+impares = []
+while True:
+    num.append(int(input('Digite um valor: ')))
+    resp = str(input('Quer continuar? [S/N] '))
+    if resp in 'Nn':
+        break
+for i, v in enumerate(num):
+    if v % 2 == 0:
+        pares.append(v)
+    elif v % 2 == 1:
+        impares.append(v)
+print('=-'*30)
+print(f'A lista completo é {num}')
+print(f'A lista de pares é {pares}')
+print(f'A lista de ímpares é {impares}')
---
+++ Mundo_3/ex083_Validating_mathematical_expressions.py
@@ -0,0 +1,22 @@
+'''
+Python Exercise 083:
+Create a program where the user types any expression that uses parentheses.
+Your application should analyze whether the entered expression 
+has the parentheses opened and closed in the correct order.
+'''
+
+expr = str(input('Digite uma expressão: '))
+pilha = []
+for simb in expr:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está válida!')
+else:
+    print('Sua expressão está errada!')
