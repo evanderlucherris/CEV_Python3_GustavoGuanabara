5fc0c37b675715a3dc3fd0f01ae2050a36b1891d
evanderlucherris <evanderlucherris@gmail.com>
2025-10-27 22:19:03 -0300

    update

--- Mundo_3/aula21_functions_part2.py
+++ Mundo_3/aula21_functions_part2.py
@@ -5,7 +5,7 @@ optional arguments to give more dynamism to python functions, variable scope, an
 '''
 def contador(i, f, p):
     """
-    -> |Faz uma contagem e mostra na tela.
+    -> Faz uma contagem e mostra na tela.
     :param i: início da contagem
     :param f: fim da contagem
     :param p: passo da contagem
@@ -16,3 +16,19 @@ def contador(i, f, p):
         print(f'{c} ', end='')
         c += p
     print('FIM!')
+
+help(contador)
+
+def somar(a=0, b=0, c=0):
+    """
+    -> Faz a soma de três valores e mostra o resultado na tela.
+    :param a: o primeiro valor
+    :param b: o segundo valor
+    :param c: o terceiro valor
+    :return: sem retorno
+    """
+    s = a + b + c
+    print(f'A soma vale {s}')
+somar(3, 2, 5)
+somar(8, 4)
+somar()
