6a54a876d44c68966906e79f8eaf7add5f63bfc2
evanderlucherris <evanderlucherris@gmail.com>
2025-10-27 23:03:55 -0300

    update

---
+++ Mundo_3/ex101_voting_functions.py
@@ -0,0 +1,6 @@
+'''
+Create a program that has a function called vote(), which will receive as a parameter a person's year of birth,
+returning a literal value indicating whether the person has their vote DENIED, OPTIONAL,
+or MANDATORY in the elections.
+'''
+
