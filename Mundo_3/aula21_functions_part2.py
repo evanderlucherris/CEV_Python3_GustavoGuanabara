9ad3806940f5fe0f61fb40888a2a9a98d4e52156
evanderlucherris <evanderlucherris@gmail.com>
2025-10-27 22:03:33 -0300

    update

--- Mundo_3/aula21_functions_part2.py
+++ Mundo_3/aula21_functions_part2.py
@@ -3,3 +3,16 @@ in this lesson, we will continue our study of functions in python,
 learning more about interactive help in python, the use of docstrings to document our functions, 
 optional arguments to give more dynamism to python functions, variable scope, and returning results.
 '''
+def contador(i, f, p):
+    """
+    -> |Faz uma contagem e mostra na tela.
+    :param i: início da contagem
+    :param f: fim da contagem
+    :param p: passo da contagem
+    :return: sem retorno
+    """
+    c = i
+    while c <= f:
+        print(f'{c} ', end='')
+        c += p
+    print('FIM!')
