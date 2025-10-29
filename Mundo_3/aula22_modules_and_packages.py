6a109e48f5500136770ace0bc0274fe5d5c8e359
evanderlucherris <evanderlucherris@gmail.com>
2025-10-28 23:18:21 -0300

    New update

---
+++ Mundo_3/aula22_modules_and_packages.py
@@ -0,0 +1,6 @@
+'''
+In this lesson, we will continue our studies of functions in Python, 
+learning how to create modules in Python and reuse our code in other projects. 
+We will also learn how to group multiple modules into a package, 
+further enhancing modularization in large Python projects.
+'''
