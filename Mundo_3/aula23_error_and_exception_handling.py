1ab1733e2b6b14a3e6104cd9f6789d77c30fe303
evanderlucherris <evanderlucherris@gmail.com>
2025-11-02 23:54:59 -0300

    new feature: error and exception handling in Python

---
+++ Mundo_3/aula23_error_and_exception_handling.py
@@ -0,0 +1,23 @@
+'''
+In this lesson, we will see how Python allows you to handle errors 
+and create responses to these exceptions. Learn how to use the 
+try-except structure in Python in a simple way.
+
+'''
+
+try:
+    a = int(input('Numerador: '))
+    b = int(input('Denominador: '))
+    r = a / b
+except (ValueError, TypeError):
+    print('Tivemos um problema com os tipos de dados que você digitou.')
+except ZeroDivisionError:
+    print('Não é possível dividir um número por zero!')
+except KeyboardInterrupt:
+    print('O usuário preferiu não informar os dados.')
+except Exception as erro:
+    print(f'O erro encontrado foi {erro.__cause__}')
+else:
+    print(f'O resultado é {r:.1f}')
+finally:
+    print('Volte sempre! Muito obrigado!')
