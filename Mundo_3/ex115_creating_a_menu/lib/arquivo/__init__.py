1a15a3372fd3fadc60c7e4a7020773cb149926b9
evanderlucherris <evanderlucherris@gmail.com>
2025-11-04 21:59:35 -0300

    update branch 'main'

---
+++ Mundo_3/ex115_creating_a_menu/lib/arquivo/__init__.py
@@ -0,0 +1,31 @@
+'''
+Let's see how to access files using Python.
+'''
+from lib.interface import *
+
+def arquivoExiste(nome):
+    try:
+        a = open(nome, 'rt')
+        a.close()
+    except FileNotFoundError:
+        return False
+    else:
+        return True
+    
+def criarArquivo(nome):
+    try:
+        a = open(nome, 'wt+')
+        a.close()
+    except:
+        print('Houve um erro na criação do arquivo!')
+    else:
+        print(f'Arquivo {nome} criado com sucesso!')
+
+def lerArquivo(nome):
+    try:
+        a = open(nome, 'rt')
+    except:
+        print('Erro ao ler o arquivo!')
+    else:
+        cabeçalho('Pessoas Cadastradas')
+        print(a.read())        
